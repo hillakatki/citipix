a0b0e4d4056e73ec5a2579eb8f2d226bf1c60c73
hillakatki <hillakatki@gmail.com>
2016-02-19 22:42:20 -0500

    FInal
    added  LA, Austin, Sydney. Also reset the form field after new photo is
    loaded.

--- js/index.js
+++ js/index.js
@@ -5,21 +5,36 @@ function changePic () {
 	//grab the entry from the input field
 	var cityName = $('#city-type').val();
 
-	//check city name
+	//check if city name is New York or New York City or NYC and swap out background image to nyc.jpg
 	if (cityName === "New York" || cityName === "New York City" || cityName === "NYC") {
 		$('body').css("background", "url('images/nyc.jpg')");
+
+	//check if city name is San Francisco or Bay Area or SF and swap out background image to sf.jpg
 	}else if (cityName === "San Francisco" || cityName === "Bay Area" || cityName === "SF") {
 		$('body').css("background", "url('images/sf.jpg')");
-	}
 
+	//check if city name is Los Angeles or LA or LAX and swap out background image to la.jpg
+	}else if (cityName === "Los Angeles" || cityName === "LA" || cityName === "LAX") {
+		$('body').css("background", "url('images/la.jpg')");
+
+	//check if city name is Austin or ATX and swap out background image to austin.jpg
+	}else if (cityName === "Austin" || cityName === "ATX") {
+		$('body').css("background", "url('images/austin.jpg')");
+
+	//check if city name is Sydney or SYD and swap out background image to sydney.jpg
+	}else if (cityName === "Sydney" || cityName === "SYD") {
+		$('body').css("background", "url('images/sydney.jpg')");
+	};
+
+	$('#city-type').val('');
 };
 
+//when the form is submitted, prevent the form from submitting and also call a function to change the picture
 
 $('form').on('submit', function(event) {
 	event.preventDefault();
 	changePic();
 });
 
-		// window.alert(cityName);
 
 
