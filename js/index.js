b15ce5adb796d772c900ae15f3c4d23fdf3e0c6b
hillakatki <hillakatki@gmail.com>
2016-02-27 16:06:52 -0500

    Added comments

--- js/index.js
+++ js/index.js
@@ -1,43 +1,50 @@
 
-var cities = ["NYC", "SF", "LA", "ATX", "SYD"];
+//setting up an array to build the dropdown
+var cities = ["Pick a city!", "NYC", "SF", "LA", "ATX", "SYD"];
 var cityName;
 
+//using a loop to build the dropdown with each element of the array
 for (var i = 0; i < cities.length; i++) {
 	$("#city-options").append('<option value='+cities[i]+'>'+cities[i]+'</option>');
 };
 
-
+//when the dropdown item is selected, this function will run
 $("#city-options").on('change', function() {
-	cityName = $("#city-options").find("option:selected").text();
+	//assigning a variable cityName to the dropdown option that is selected
+	cityName = $("#city-options").find("option:selected").val();
 	console.log(cityName);
+	//running the function changePic
 	changePic();
 });
 
+//changePic will change the background photo depending on the cityName
 function changePic () {
-
-
+	
+	//check if city name is NYC and swap out background image to nyc.jpg
 	if (cityName ===  "NYC") {
-	$('body').css("background", "url('images/nyc.jpg')");
+		$('body').css("background", "url('images/nyc.jpg')");
 
-	//check if city name is San Francisco or Bay Area or SF and swap out background image to sf.jpg
+	//check if city name is SF and swap out background image to sf.jpg
 	}else if (cityName === "SF") {
 		$('body').css("background", "url('images/sf.jpg')");
 
-	//check if city name is Los Angeles or LA or LAX and swap out background image to la.jpg
+	//check if city name is LA and swap out background image to la.jpg
 	}else if (cityName === "LA") {
 		$('body').css("background", "url('images/la.jpg')");
 
-	//check if city name is Austin or ATX and swap out background image to austin.jpg
+	//check if city name is ATX and swap out background image to austin.jpg
 	}else if (cityName === "ATX") {
 		$('body').css("background", "url('images/austin.jpg')");
 
-	//check if city name is Sydney or SYD and swap out background image to sydney.jpg
+	//check if city name is SYD and swap out background image to sydney.jpg
 	}else if (cityName === "SYD") {
 		$('body').css("background", "url('images/sydney.jpg')");
-	};
+	
+	//if city name is none of the above, swap out background image to original image
+	}else
+		$('body').css("background", "url('images/citipix_skyline.jpg')");
 
-	$('#city-type').val('');
-}
+};
 
 // function changePic () {
 
